4dddca8527801dcfea4fc6cacdc3bcdb7e1b525b
Magneseus <matt.bmayer@gmail.com>
2018-05-15 22:07:09 -0400

    More work on the Expansion functionality

--- Assets/Bisec.cs
+++ Assets/Bisec.cs
@@ -12,28 +12,159 @@ public class Bisec : MonoBehaviour {
 
     private Mesh targetMesh;
 
-	// Use this for initialization
+    private int[][] lineLookupTable;
+
+	// 
 	void Start () {
         targetMesh = GetComponent<MeshFilter>().mesh;
 
-        Debug.Log("Sub mesh count: " + targetMesh.subMeshCount);
-        Debug.Log("Vertex count: " + targetMesh.vertexCount);
+        // No Mesh target
+        if (targetMesh == null)
+        {
+            throw new MissingComponentException("No MeshFilter component to bisect.");
+        }
+        else
+        {
+            NewMeshBuild();
+        }
     }
-	
-	// Update is called once per frame
-	void Update () {
-	}
+
+    private void NewMeshBuild()
+    {
+        lineLookupTable = new int[targetMesh.vertexCount][];
+        for (int i = 0; i < lineLookupTable.Length; i++)
+        {
+            lineLookupTable[i] = new int[targetMesh.vertexCount];
+            for (int j = 0; j < lineLookupTable[i].Length; j++)
+            {
+                lineLookupTable[i][j] = -1;
+            }
+        }
+    }
 
     /*
-     * When bisecting, should achieve a O(n) time, but could possibly reduce
+     * When expanding, should achieve a O(n) time, but could possibly reduce
      * by treating like a graph, jumping through nodes and only taking ones
      * that have a chance of intersecting. O(log n)?
      * 
-     * Will have a edge case where line is laying on the plane.
+     * Will have a edge case where line is laying on the plane?
      */
-    void Bisect(Plane bisectPlane)
+    public void Expand(b_Plane bisectPlane, Vector3 translation)
     {
-       
+        if (targetMesh == null)
+            return;
+
+        // Unity plane for ease of use
+        Plane uPlane = new Plane(bisectPlane.normal, bisectPlane.location);
+        Vector3 translationSide = bisectPlane.location + translation;
+
+        // Mesh Information
+        Vector3[] verts = targetMesh.vertices;
+        int[] triangles = targetMesh.triangles;
+
+        // New Mesh Additions
+        List<Vector3> newVerts = new List<Vector3>();
+        List<int> newTriangles = new List<int>();
+
+        // Intersections
+        Vector3[] intersectons = new Vector3[3];
+
+        // Points
+        int[] pi = new int[3];
+        Vector3[] pv = new Vector3[3];
+
+        // Iterate through all triangles
+        for (int i = 0; i < triangles.Length; i += 3)
+        {
+            // Get intersection of triangles and plane
+            pi[0] = triangles[i];
+            pi[1] = triangles[i + 1];
+            pi[2] = triangles[i + 2];
+            pv[0] = verts[pi[0]];
+            pv[1] = verts[pi[1]];
+            pv[2] = verts[pi[2]];
+
+            // Iterate through each line
+            for (int j = 0; j < 3; j++)
+            {
+                // Check if the line has been bisected previously
+                int jpo = (j + 1) % 3;
+                int ind = LineIntersectLookup(pi[j], pi[jpo]);
+
+                // If done previously and no intersection, skip
+                if (ind == -2)
+                {
+                    continue;
+                }
+                // If not done previously
+                else if (ind == -1)
+                {
+                    // Look for an intersection
+                    if (PlaneSegmentIntersection(
+                        bisectPlane, 
+                        pv[pi[j]], 
+                        pv[pi[jpo]], 
+                        out intersectons[j]) == 1)
+                    {
+                        // Set the lookup table for intersects
+                        SetLineIntersect(pi[j], pi[jpo], verts.Length + newVerts.Count);
+
+                        // Generate new vertices
+                        newVerts.Add(new Vector3(
+                            intersectons[j].x, 
+                            intersectons[j].y, 
+                            intersectons[j].z));
+                        newVerts.Add(new Vector3(
+                            intersectons[j].x + translation.x, 
+                            intersectons[j].y + translation.y,
+                            intersectons[j].z + translation.z));
+
+                        // Move original vertex that is on the "expanding" side
+                        // by specified translation
+                        if (uPlane.SameSide(pv[j], translationSide))
+                        {
+                            pv[j] += translation;
+                        }
+                        else
+                        {
+                            pv[jpo] += translation;
+                        }
+                    }
+                    // If no bisection, continue
+                    else
+                    {
+                        SetLineIntersect(pi[j], pi[jpo], -2);
+                        intersectons[j] = Vector3.zero;
+                        continue;
+                    }
+                }
+                
+                // If done previously, with an intersection
+                
+            } // END OF LINE ITERATION
+        }
+    }
+
+    /*
+     * Checks if a line has been previously bisected during an expansion operation.
+     * Returns -1 if no bisection occurred.
+     * 
+     * Returns the positive integer representing the index of the newly created
+     * vertex that was at the location of the bisection.
+     * 
+     * The returned index + 1 represents the index of the secondary vertex created
+     * and translated.
+     */
+    private int LineIntersectLookup(int vertInd1, int vertInd2)
+    {
+        return lineLookupTable[vertInd1][vertInd2];
+    }
+
+    // Make sure to use LineIntersectLookup first
+    private void SetLineIntersect(int vertInd1, int vertInd2, int newVertInd)
+    {
+        lineLookupTable[vertInd1][vertInd2] = newVertInd;
+        lineLookupTable[vertInd2][vertInd1] = newVertInd;
     }
 
     // 0 -> no intersection
---
+++ Assets/WorldController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour
+{
+    public Bisec cube;
+    public GameObject planeOne;
+    public GameObject planeTwo;
+
+    private Vector3 translate;
+    private Bisec.b_Plane plane_;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.E))
+        {
+            translate = planeOne.transform.position - planeTwo.transform.position;
+
+            plane_.location = planeOne.transform.position;
+            plane_.normal = Vector3.up;
+            plane_.normal = planeOne.transform.rotation * plane_.normal;
+
+            cube.Expand(plane_, translate);
+        }
+	}
+}
